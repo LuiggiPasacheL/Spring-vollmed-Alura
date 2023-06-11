75d0bdf7ec56cfbafbf6c5fc30a4c71da68da7cf
Luiggi Pasache <luiggi.pasache@unmsm.edu.pe>
2023-06-11 16:05:23 -0500

    Aplicando buenas practicas en API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,30 +21,45 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
-    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
-        medicoRepository.save(new Medico(datosRegistroMedico));
+    public ResponseEntity<DatosRespuestaMedico> registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico,
+                                          UriComponentsBuilder uriComponentsBuilder){
+        Medico medico = medicoRepository.save(new Medico(datosRegistroMedico));
+        DatosRespuestaMedico datosRespuestaMedico = new DatosRespuestaMedico(medico);
+        URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(url).body(datosRespuestaMedico); // Retorna un 201 (Created)
+        // Retorna url de la entidad creada en la cabecera
+        // Retorna la entidad creada en el cuerpo
     }
 
     @GetMapping
     // @PageableDefault -> Cambia los valores por defecto de la interfaz Pageable
-    public Page<DatosListadoMedico> listadoMedico(@PageableDefault(size = 2) Pageable paginacion) {
+    public ResponseEntity<Page<DatosListadoMedico>> listadoMedico(@PageableDefault(size = 2) Pageable paginacion) {
         // Convierte toda la lista de medico a lista de DatosListadoMedico
         // return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
-        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+        return ResponseEntity.ok(medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new)); // Retorna un 200 (Ok) con la pagina
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DatosRespuestaMedico> obtenerMedico(@PathVariable Long id) {
+        Medico medico = medicoRepository.getReferenceById(id);
+        DatosRespuestaMedico datosRespuestaMedico = new DatosRespuestaMedico(medico);
+        return ResponseEntity.ok(datosRespuestaMedico); // Retorna un 200 (Ok) con la entidad
     }
 
     @PutMapping
     @Transactional // <- Libera la transaccion cuando se termina el método
-    public void actualizarMedico(@RequestBody DatosActualizarMedico datosActualizarMedico) {
+    public ResponseEntity actualizarMedico(@RequestBody DatosActualizarMedico datosActualizarMedico) {
         Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
         medico.actualizarDatos(datosActualizarMedico);
+        return ResponseEntity.ok(new DatosRespuestaMedico(medico)); // Retorna un 200 (Ok) con el médico editado
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void eliminarMedico(@PathVariable Long id) {
+    public ResponseEntity eliminarMedico(@PathVariable Long id) {
         Medico medico = medicoRepository.getReferenceById(id);
         // medicoRepository.delete(medico); // -> Borrado fisico de la base de datos
         medico.desactivarMedico(); // -> Borrado lógico
+        return ResponseEntity.noContent().build(); // Retorna un 204 (No content)
     }
 }
--- src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -13,4 +13,7 @@ public record DatosDireccion(
         String numero,
         @NotBlank
         String complemento) {
+    public DatosDireccion(Direccion direccion) {
+        this(direccion.getCalle(), direccion.getDistrito(), direccion.getCiudad(), direccion.getNumero(), direccion.getComplemento());
+    }
 }
